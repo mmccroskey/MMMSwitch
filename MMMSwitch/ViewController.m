6e29f7d0caf83a84e42b1db001345e1f9f21d81d
Matthew McCroskey <matthew.mccroskey@gmail.com>
2014-10-04 20:27:29 -0500

    Add Color Square to Show Switch States

--- MMMSwitch/ViewController.m
+++ MMMSwitch/ViewController.m
@@ -13,6 +13,7 @@
 
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *switchWidthConstraint;
 @property (weak, nonatomic) IBOutlet MMMSwitch *theSwitch;
+@property (weak, nonatomic) IBOutlet UIView *colorSquare;
 
 @end
 
@@ -24,35 +25,48 @@
     
     self.theSwitch.stateDidChangeHandler = ^(MMMSwitchState newState)
     {
+        CGFloat colorLevel;
+        
         switch (newState)
         {
             case MMMSwitchStateOff:
+                colorLevel = (0.0f/5.0f);
                 NSLog(@"The switch is off");
                 break;
                 
             case MMMSwitchStateSelectedUnpressedOff:
+                colorLevel = (1.0f/5.0f);
                 NSLog(@"The switch is selected, unpressed, and off");
                 break;
                 
             case MMMSwitchStateSelectedPressedOff:
+                colorLevel = (2.0f/5.0f);
                 NSLog(@"The switch is selected, pressed, and off");
                 break;
                 
             case MMMSwitchStateSelectedUnpressedOn:
+                colorLevel = (3.0f/5.0f);
                 NSLog(@"The switch is selected, unpressed, and on");
                 break;
                 
             case MMMSwitchStateSelectedPressedOn:
+                colorLevel = (4.0f/5.0f);
                 NSLog(@"The switch is selected, pressed, and on");
                 break;
                 
             case MMMSwitchStateOn:
+                colorLevel = (5.0f/5.0f);
                 NSLog(@"The switch is on");
                 break;
                 
             default:
                 break;
         }
+        
+        [UIView animateWithDuration:0.1f animations:^
+        {
+            self.colorSquare.backgroundColor = [UIColor colorWithRed:colorLevel green:colorLevel blue:colorLevel alpha:1.0f];
+        }];
     };
     
 //    [self performSelector:@selector(increaseWidth) withObject:nil afterDelay:3.0f];.
